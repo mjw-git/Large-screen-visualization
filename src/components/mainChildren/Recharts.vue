0bfedde09a8063be804a056dd68acdd332260d6f
mjw-git <547783781@qq.com>
2020-09-18 17:44:14 +0800

    新增部分图标

--- src/components/mainChildren/Recharts.vue
+++ src/components/mainChildren/Recharts.vue
@@ -1,20 +1,46 @@
 <template>
-    <div class="r_con">
-        <div class="box" ref="echart1"></div>
+  <div class="r_con">
+    <div class="box" ref="echart1"></div>
+    <div class="box1" ref="echart2"></div>
+    <div class="box2" ref="echart3"></div>
+    <div class="box3" ref="echart4">
+           <div class="heads">
+               <div class="head">排名</div>
+               <div class="head">公司名称</div>
+               <div class="head">收益</div>
+           </div>
+           <div class="items" v-for="(item,index) in listData" :key="index">
+               <div class="item">{{index+1}}</div>
+               <div class="item">xxxx公司{{index+1}}</div>
+               <div class="item">{{(10-index+1)*10}}万元</div>
+           </div>
     </div>
+  </div>
 </template>
 
 <script>
-import {option1} from '../../config/Recharts.config'
-    export default {
-        mounted () {
-            let myChart = this.$echarts.init(this.$refs.echart1);
-            myChart.setOption(option1);
-        },
-          
-    }
+import vueSeamlessScroll from "vue-seamless-scroll";
+import { option1, option2, option3 } from "../../config/Recharts.config";
+export default {
+  mounted() {
+    let myChart = this.$echarts.init(this.$refs.echart1);
+    myChart.setOption(option1);
+    let myChart2 = this.$echarts.init(this.$refs.echart2);
+    myChart2.setOption(option2);
+    let myChart3 = this.$echarts.init(this.$refs.echart3);
+    myChart3.setOption(option3);
+  },
+  components: {
+    vueSeamlessScroll,
+  },
+  data() {
+      return {
+          listData:[1,2,3,4,5,6,7,8,9,10]
+      }
+  },
+};
 </script>
 
 <style lang="less" scoped>
-@import '../../assets/css/right.less';
+@import "../../assets/css/right.less";
 </style>
--- src/config/Recharts.config.js
+++ src/config/Recharts.config.js
@@ -13,7 +13,7 @@ export const option1 = {
             type: 'liquidFill',
             radius: '65%',
             center: ['50%', '50%'],
-            data: [0.3, 0.3, 0.3], // data个数代表波浪数
+            data: [0.8, 0.8, 0.8], // data个数代表波浪数
             backgroundStyle: {
                 borderWidth: 1,
                 color: 'rgb(255,0,255,0.1)'
@@ -130,3 +130,233 @@ export const option1 = {
 
     ]
 }
+export const option2 = {
+    backgroundColor: '',
+    grid:{
+        right:'20%',
+        containLabel: true
+    },
+    tooltip: {
+        trigger: 'item',
+        formatter: "{b} : {c} ({d}%)"
+    },
+
+    visualMap: {
+        show: false,
+        min: 500,
+        max: 600,
+        inRange: {
+            //colorLightness: [0, 1]
+        }
+    },
+    series: [{
+        type: 'pie',
+        radius: '50%',
+        center: ['50%', '50%'],
+        color: ['rgb(131,249,103)', '#FBFE27', '#FE5050', '#1DB7E5','#FBFE27'], //'#FBFE27','rgb(11,228,96)','#FE5050'
+        data: [{
+                value: 585,
+                name: '优秀'
+            },
+            {
+                value: 410,
+                name: '良好'
+            },
+            {
+                value: 274,
+                name: '适中'
+            },
+            {
+                value: 235,
+                name: '较差'
+            },{
+                value:50,
+                name:'极差'
+            }
+        ].sort(function(a, b) {
+            return a.value - b.value
+        }),
+        roseType: 'radius',
+
+        label: {
+            normal: {
+                formatter: ['{c|{c}次}', '{b|{b}}'].join('\n'),
+                rich: {
+                    c: {
+                        color: 'rgb(241,246,104)',
+                        fontSize: 12,
+                       
+                        lineHeight: 0
+                    },
+                    b: {
+                        color: 'rgb(1,234,254)',
+                        fontSize: 12,
+                        height: 25
+                    },
+                },
+            }
+        },
+        labelLine: {
+            normal: {
+                lineStyle: {
+                    color: 'rgb(98,137,169)',
+                },
+                smooth: 0.2,
+                length: 10,
+                length2: 20,
+
+            }
+        },
+        itemStyle: {
+            normal: {
+                shadowColor: 'rgba(0, 0, 0, 0.8)',
+                shadowBlur: 50,
+            }
+        }
+    }]
+};
+var colorArray = [
+    {
+        top: '#ffa800', //黄
+        bottom: 'rgba(11,42,84,.3)'
+    }, {
+        top: '#1ace4a', //绿
+        bottom: 'rgba(11,42,84, 0.3)'
+    },
+    {
+        top: '#4bf3ff', //蓝
+        bottom: 'rgba(11,42,84,.3)'
+    }, {
+        top: '#4f9aff', //深蓝
+        bottom: 'rgba(11,42,84,.3)'
+    },
+    {
+        top: '#b250ff', //粉
+        bottom: 'rgba(11,42,84,.3)'
+    }
+];
+export const option3 = {
+   
+     tooltip: {
+          show: true,
+          formatter: "{b}:{c}"
+        },
+   grid: {
+          left: '5%',
+          top: '12%',
+          right: '10%',
+          bottom: '8%',
+          containLabel: true
+        },
+  
+    xAxis: {
+        type: 'value',
+        show:false,
+        position: 'top',
+        axisTick: {
+            show: false
+        },
+        axisLine: {
+            show: false,
+            lineStyle: {
+                color: '#fff',
+            }
+        },
+        splitLine: {
+            show: false
+        },
+    },
+    yAxis: [{
+            type: 'category',
+            axisTick: {
+                show: false,
+                alignWithLabel: false,
+                length: 5,
+
+            },
+            "splitLine": { //网格线
+                "show": false
+            },
+            inverse: 'true', //排序
+            axisLine: {
+                show: false,
+                lineStyle: {
+                    color: '#fff',
+                }
+            },
+            data: ['1月', '2月', '3月', '4月', '5月']
+        }
+
+    ],
+    series: [{
+            name: '能耗值',
+            type: 'bar',
+                label: {
+                normal: {
+                  show: true,
+                  position: 'right',
+                  formatter: '{c}万元',
+                  textStyle: {
+                    color: 'white' //color of value
+                  }
+                }
+              },
+            itemStyle: {
+                normal: {
+                    show: true,
+                    color: function(params) {
+                        let num = colorArray.length;
+                        return {
+                            type: 'linear',
+                            colorStops: [{
+                                offset: 0,
+                                color: colorArray[params.dataIndex % num].bottom
+                            }, {
+                                offset: 1,
+                                color: colorArray[params.dataIndex % num].top
+                            }, {
+                                offset: 0,
+                                color: colorArray[params.dataIndex % num].bottom
+                            }, {
+                                offset: 1,
+                                color: colorArray[params.dataIndex % num].top
+                            }, {
+                                offset: 0,
+                                color: colorArray[params.dataIndex % num].bottom
+                            }, {
+                                offset: 1,
+                                color: colorArray[params.dataIndex % num].top
+                            }, {
+                                offset: 0,
+                                color: colorArray[params.dataIndex % num].bottom
+                            }, {
+                                offset: 1,
+                                color: colorArray[params.dataIndex % num].top
+                            }, {
+                                offset: 0,
+                                color: colorArray[params.dataIndex % num].bottom
+                            }, {
+                                offset: 1,
+                                color: colorArray[params.dataIndex % num].top
+                            }, {
+                                offset: 0,
+                                color: colorArray[params.dataIndex % num].bottom
+                            }, {
+                                offset: 1,
+                                color: colorArray[params.dataIndex % num].top
+                            }],
+                            //globalCoord: false
+                        }
+                    },
+                    barBorderRadius: 70,
+                    borderWidth: 0,
+                    borderColor: '#333',
+                }
+            },
+            barGap: '0%',
+            barCategoryGap: '50%',
+            data: [60, 132, 89, 134, 60]
+        }
+
+    ]
+};
